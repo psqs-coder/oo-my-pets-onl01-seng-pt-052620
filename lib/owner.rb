edf2480bb3bb10aec7b6cf6a6f938ff726ca7e15
Bruce Parker <detailsharks@gmail.com>
2020-06-27 20:50:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,14 +45,14 @@ class Owner
       self.cats.delete(cat)
       cat.owner = nil
       cat.mood = "nervous"
-  end
+    end
   
   def sell_pets
     sell.dogs.each do |dog|
       self.dogs.delete(dog)
       dog.owner = nil
       dog.mood = "nervous"
-  end
+    end
   
   def list_pets
     number_of_dogs = self.dogs.count
