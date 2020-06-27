e299d524ea515fa7db12c4fe151d43e67f48fa91
Bruce Parker <detailsharks@gmail.com>
2020-06-27 19:45:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    end
+    
+   def self.all 
+     @@all
+     end
+
+   def self.count
+     self.all.length
+     end
+    
+  def say_species
+    "I am a #{@species}."
+    end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
 end
+    
+  
